39f3cb2f2e3e8a046c64ccb643e3fde1abe15e46
Kinko7 <kion7kion@gmail.com>
2020-11-12 08:19:06 -0300

    LastRevision

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -1,93 +1,130 @@
 // No cambies los nombres de las funciones.
-
 function devolverPrimerElemento(array) {
-  // Devuelve el primer elemento de un array
-  // Tu código:
+    // Devuelve el primer elemento de un array
+    // Tu código:
+    return array[0];
 }
 
 function devolverUltimoElemento(array) {
-  // Devuelve el último elemento de un array
-  // Tu código:
+    // Devuelve el último elemento de un array
+    // Tu código:
+    return array[array.length - 1];
 }
 
 function obtenerLargoDelArray(array) {
-  // Devuelve el largo de un array
-  // Tu código:
+    // Devuelve el largo de un array
+    // Tu código:
+    return array.length;
 }
 
 function incrementarPorUno(array) {
-  // "array" debe ser una matriz de enteros (int/integers)
-  // Aumenta cada entero por 1
-  // y devuelve el array
-  // Tu código:
+    // "array" debe ser una matriz de enteros (int/integers)
+    // Aumenta cada entero por 1
+    // y devuelve el array
+    // Tu código:
+    var nuevoArray = [];
+    for (var i = 0; i < array.length; i++) {
+        nuevoArray[i] = array[i] + 1;
+    }
+    return nuevoArray;
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
-  // Añade el "elemento" al final del array
-  // y devuelve el array
-  // Tu código:
+    // Añade el "elemento" al final del array
+    // y devuelve el array
+    // Tu código:
+    array.push(elemento);
+    return array;
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
-  // Añade el "elemento" al comienzo del array
-  // y devuelve el array
-  // Pista: usa el método `.unshift`
-  // Tu código:
+    // Añade el "elemento" al comienzo del array
+    // y devuelve el array
+    // Pista: usa el método `.unshift`
+    // Tu código:
+    array.unshift(elemento);
+    return array;
 }
 
 function dePalabrasAFrase(palabras) {
-  // "palabras" es un array de strings/cadenas
-  // Devuelve un string donde todas las palabras estén concatenadas
-  // con espacios entre cada palabra
-  // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
-  // Tu código:
+    // "palabras" es un array de strings/cadenas
+    // Devuelve un string donde todas las palabras estén concatenadas
+    // con espacios entre cada palabra
+    // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
+    // Tu código:
+    return palabras.join(" ");
 }
 
 function arrayContiene(array, elemento) {
-  // Comprueba si el elemento existe dentro de "array"
-  // Devuelve "true" si está, o "false" si no está
-  // Tu código:
+    // Comprueba si el elemento existe dentro de "array"
+    // Devuelve "true" si está, o "false" si no está
+    // Tu código:
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === elemento) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function agregarNumeros(numeros) {
-  // "array" debe ser una matriz de enteros (int/integers)
-  // Suma todos los enteros y devuelve el valor
-  // Tu código:
+    // "array" debe ser una matriz de enteros (int/integers)
+    // Suma todos los enteros y devuelve el valor
+    // Tu código:
+    var Suma = 0;
+    for (var i = 0; i < numeros.length; i++) {
+        Suma = Suma + numeros[i];
+    }
+    return Suma;
 }
 
 function promedioResultadosTest(resultadosTest) {
-  // "resultadosTest" debe ser una matriz de enteros (int/integers)
-  // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
-  // Tu código:
+    // "resultadosTest" debe ser una matriz de enteros (int/integers)
+    // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
+    // Tu código:
+    SumaN = 0;
+    promedioResultadosTest = [3, 7, 9, 10, 5];
+    proemdio = 0;
+    for (var i = 0; i < resultadosTest.length; i++) {
+        SumaN = SumaN + resultadosTest[i];
+    } {
+        promedio = SumaN / resultadosTest.length;
+    }
+    return promedio;
 }
 
 function numeroMasGrande(numeros) {
-  // "numeros" debe ser una matriz de enteros (int/integers)
-  // Devuelve el número más grande
-  // Tu código:
+    // "numeros" debe ser una matriz de enteros (int/integers)
+    // Devuelve el número más grande
+    // Tu código: 
+    return Math.max(...numeros);
 }
 
 function multiplicarArgumentos() {
-  // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
-  // Si no se pasan argumentos devuelve 0
-  // Si se pasa un argumento, simplemente devuélvelo
-  // Tu código:
+    // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
+       // Si no se pasan argumentos devuelve 0
+       // Si se pasa un argumento, simplemente devuélvelo
+    // Tu código:
+    if (arguments.length < 1) return 0;
+    var total = 1;
+    for (var i = 0; i < arguments.length; i++) {
+        total = total * arguments[i];
+    }
+    return total;
 }
-
 // No modificar nada debajo de esta línea
 // --------------------------------
-
 module.exports = {
-  devolverPrimerElemento,
-  devolverUltimoElemento,
-  obtenerLargoDelArray,
-  incrementarPorUno,
-  agregarItemAlFinalDelArray,
-  agregarItemAlComienzoDelArray,
-  dePalabrasAFrase,
-  arrayContiene,
-  agregarNumeros,
-  promedioResultadosTest,
-  numeroMasGrande,
-  multiplicarArgumentos,
+    devolverPrimerElemento,
+    devolverUltimoElemento,
+    obtenerLargoDelArray,
+    incrementarPorUno,
+    agregarItemAlFinalDelArray,
+    agregarItemAlComienzoDelArray,
+    dePalabrasAFrase,
+    arrayContiene,
+    agregarNumeros,
+    promedioResultadosTest,
+    numeroMasGrande,
+    multiplicarArgumentos,
 };
--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,32 +1,55 @@
 // Do not change any of the function names
-
 function invocarCallback(cb) {
-  // Invoca al callback `cb`
+    // Invoca al callback `cb`, es una
+    //funcion que la pasan como argumento.
+    cb();
 }
 
 function sumarArray(numeros, cb) {
-  // Suma todos los números enteros (int/integers) de un array ("numeros")
-  // Pasa el resultado a `cb`
-  // No es necesario devolver nada
+    // Suma todos los números enteros (int/integers) de un array ("numeros")
+    // Pasa el resultado a `cb`
+    // No es necesario devolver nada
+    //numeros es un array y un callback que es funcion
+    // ir pasando argumerntos dela array a cb
+    // le paso al cb el total de la suma
+    var suma = numeros.reduce(function(ele, nu) {
+        return ele + nu;
+    }, 0);
+    cb(suma);
 }
 
 function forEach(array, cb) {
-  // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
-  // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+    // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
+    // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+    //for ( let i = 0, i<array.length;i++){
+    //cb(array[i]); //le voy pasando los argumentos
+    for (var i = 0; i < array.length; i++) {
+        cb(array[i]);
+    }
 }
 
 function map(array, cb) {
-  // Crea un nuevo array
-  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
-  // El nuevo array debe tener la misma longitud que el array del argumento
+    // Crea un nuevo array
+    // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
+    // El nuevo array debe tener la misma longitud que el array del argumento
+    // var array = [1,2,3,4,5]
+    // function cb (e) {console.log(e)}
+    // nuevoarray = []
+    // var nuevoArray = [];
+    // for(var i = 0; i < array.length; i++) {
+    //   nuevoArray.push(cb(array[i]));
+    //   nuevoArray[i] = cb(array[i]);
+    // }
+    var nuevoArray = array.map(function(elemento) {
+        return cb(elemento);
+    });
+    return nuevoArray;
 }
-
-// No modificar nada debajo de esta línea
+// No; i< array.length;jo de esta línea
 // --------------------------------
-
 module.exports = {
-  invocarCallback,
-  sumarArray,
-  forEach,
-  map,
+    invocarCallback,
+    sumarArray,
+    forEach,
+    map,
 };
